1abcc978800d48d9af303a43398b6be1c2d8f8b9
M AKIF <makifhussainmn@gmail.com>
2023-03-19 19:28:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// console.log (100);
+
+
+// var mulristri = '
+// akscdcmd smdkkk
+//  ssssss
+//  ssssss
+// slos
+// ';
+// console.log (mulristri )
+
+
+ 
+// let firstName = "M Akif" ;
+// let lastName = " Hussain " ;
+// // let fullName = firstName +  lastName;
+
+// console.log (firstName+lastName);
+ 
+
+
+ 
+// let names = ["ahmed"," akif","noor"] ;
+// //  let marks = [ 100,52,87]
+// console.log ( names [ 2])
+
+
+// let studentsName = [];
+// studentsName[0]="Akif";
+
+// studentsName[1]="noor";
+
+// studentsName[3]="husband wife";
+
+
+
+
+console.log (studentsName[ 3] )
